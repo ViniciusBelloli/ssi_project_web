8d28e8796ed48f074bc4ad2b5b6067269c343fda
Vinicius Belloli <vbelloli@outlook.com>
2022-12-17 22:23:49 +0000

    fix error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 import dns from 'dns'
 
 dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     port: 9000,
     strictPort: true,
     host: true,
+    https: true,
     hmr: {
       host: '172.22.56.176',
       port: 3000,
